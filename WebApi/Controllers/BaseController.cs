beeac8dde2648a390461e6ef7e8387a9e9201176
Marianne Mateo <mariannejmb18@gmail.com>
2022-02-24 11:58:27 -0400

    Agregar archivos de proyecto.

---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public Account Account => (Account)HttpContext.Items["Account"];
+    }
+}
